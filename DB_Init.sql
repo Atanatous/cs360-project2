119965f51597212f45beeb7884aaf2f76f635883
Minseong Choi <chanm1996@gmail.com>
2018-12-02 18:32:00 +0900

    Trim the codes

--- DB_Init.sql
+++ DB_Init.sql
@@ -7,12 +7,11 @@ GRANT ALL PRIVILEGES ON cs360_team10.* TO 'tester'@'localhost';
 ALTER DATABASE cs360_team10 DEFAULT CHARACTER SET euckr;
 
 CREATE TABLE IF NOT EXISTS TRAINER (
-    uid         MEDIUMINT   NOT NULL AUTO_INCREMENT,
-	user_id     VARCHAR(32) NOT NULL UNIQUE,
+	user_id     VARCHAR(32)  NOT NULL ,
 	user_pw     VARCHAR(300) NOT NULL,
-	gold        INT         DEFAULT 0,
-	nickname    VARCHAR(32) NOT NULL,
-	PRIMARY KEY (uid)
+	gold        INT          DEFAULT 0,
+	nickname    VARCHAR(32)  NOT NULL,
+	PRIMARY KEY (user_id)
 );
 
 CREATE TABLE IF NOT EXISTS POKEMON (
@@ -30,13 +29,13 @@ CREATE TABLE IF NOT EXISTS POKEMON (
 );
 
 CREATE TABLE IF NOT EXISTS POSSESS (
-    uid         MEDIUMINT   NOT NULL,
+    user_id     VARCHAR(32) NOT NULL,
     poke_no     MEDIUMINT   NOT NULL,
     skill1      VARCHAR(20) NOT NULL,
     skill2      VARCHAR(20),
     map         VARCHAR(20),
     atk         INT         NOT NULL,
-    FOREIGN KEY (uid) REFERENCES TRAINER(uid),
+    FOREIGN KEY (user_id) REFERENCES TRAINER(user_id),
     FOREIGN KEY (poke_no) REFERENCES POKEMON(poke_no)
 );
 
